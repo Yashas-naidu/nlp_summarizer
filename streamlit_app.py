fe45260ee9c02454b6dd765e0848f98c068dac3c
unknown <yashashsn555@gmail.com>
2024-04-03 12:21:56 +0530

    added bart model

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,152 @@
+# import streamlit as st
+# import time
+# from transformers import T5ForConditionalGeneration, T5Tokenizer
+# import nltk
+# from nltk.tokenize import word_tokenize, sent_tokenize
+# from nltk.corpus import stopwords
+# import readtime
+# import textstat
+# from io import StringIO
+
+# st.set_page_config(page_title="SYNTHIA", 
+#                    page_icon=":robot_face:",
+#                    layout="wide",
+#                    initial_sidebar_state="expanded"
+#                    )
+
+# def p_title(title):
+#     st.markdown(f'<h3 style="text-align: left; color:#F63366; font-size:28px;">{title}</h3>', unsafe_allow_html=True)
+
+# st.sidebar.header('I want to :crystal_ball:')
+# nav = st.sidebar.radio('',['Summarize text','Analyze text'])
+# st.sidebar.write('')
+# st.sidebar.write('')
+# st.sidebar.write('')
+# st.sidebar.write('')
+# st.sidebar.write('')
+
+# if nav == 'Summarize text':    
+#     st.markdown("<h4 style='text-align: center; color:grey;'>Accelerate knowledge with Research et al &#129302;</h4>", unsafe_allow_html=True)
+#     st.text('')
+#     p_title('Summarize')
+#     st.text('')
+
+#     source = st.radio("How would you like to start? Choose an option below",
+#                           ("I want to input some text", "I want to upload a file"))
+#     st.text('')
+
+#     if source == 'I want to input some text':
+#         input_su = st.text_area("Input your text in English (between 1,000 and 10,000 characters)", height=330)
+#         if st.button('Summarize'):
+#             if len(input_su) < 1000:
+#                 st.error('Please enter a text in English of minimum 1,000 characters')
+#             else:
+#                 with st.spinner('Processing...'):
+#                     time.sleep(2)
+#                     model = T5ForConditionalGeneration.from_pretrained("t5-small")
+#                     tokenizer = T5Tokenizer.from_pretrained("t5-small")
+#                     inputs = tokenizer.encode("summarize: " + input_su, return_tensors="pt", max_length=512, truncation=True)
+#                     summary_ids = model.generate(inputs, max_length=10000, min_length=1000, length_penalty=2.0, num_beams=4, early_stopping=True)
+#                     t5_summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+#                     st.markdown('___')
+#                     st.write('T5 Model')
+#                     st.success(t5_summary)
+#                     st.balloons()
+
+#     if source == 'I want to upload a file':
+#         file = st.file_uploader('Upload your file here',type=['txt'])
+#         if file is not None:
+#             with st.spinner('Processing...'):
+#                     time.sleep(2)
+#                     stringio = StringIO(file.getvalue().decode("utf-8"))
+#                     string_data = stringio.read()
+#                     if len(string_data) < 1000 or len(string_data) > 10000:
+#                         st.error('Please upload a file between 1,000 and 10,000 characters')
+#                     else:
+#                         model = T5ForConditionalGeneration.from_pretrained("t5-small")
+#                         tokenizer = T5Tokenizer.from_pretrained("t5-small")
+#                         inputs = tokenizer.encode("summarize: " + string_data, return_tensors="pt", max_length=512, truncation=True)
+#                         summary_ids = model.generate(inputs, max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
+#                         t5_summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+#                         st.markdown('___')
+#                         st.write('T5 Model')
+#                         st.success(t5_summary)
+#                         st.balloons()
+
+# if nav == 'Analyze text':
+#     st.markdown("<h4 style='text-align: center; color:grey;'>Accelerate knowledge with Research-et-all &#129302;</h4>", unsafe_allow_html=True)
+#     st.text('')
+#     p_title('Analyze text')
+#     st.text('')
+
+#     a_example = "Artificial intelligence (AI) is intelligence demonstrated by machines, as opposed to the natural intelligence displayed by humans or animals. Leading AI textbooks define the field as the study of 'intelligent agents': any system that perceives its environment and takes actions that maximize its chance of achieving its goals. Some popular accounts use the term 'artificial intelligence' to describe machines that mimic cognitive functions that humans associate with the human mind, such as learning and problem solving, however this definition is rejected by major AI researchers. AI applications include advanced web search engines, recommendation systems (used by YouTube, Amazon and Netflix), understanding human speech (such as Siri or Alexa), self-driving cars (such as Tesla), and competing at the highest level in strategic game systems (such as chess and Go). As machines become increasingly capable, tasks considered to require intelligence are often removed from the definition of AI, a phenomenon known as the AI effect. For instance, optical character recognition is frequently excluded from things considered to be AI, having become a routine technology."
+
+#     source = st.radio("How would you like to start? Choose an option below",
+#                           ("I want to input some text", "I want to upload a file"))
+#     st.text('')
+
+#     if source == 'I want to input some text':
+#         input_me = st.text_area("Input your text in English (maximum of 10,000 characters)", value=a_example, height=330)
+#         if st.button('Analyze'):
+#             if len(input_me) > 10000:
+#                 st.error('Please enter a text in English of maximum 1,000 characters')
+#             else:
+#                 with st.spinner('Processing...'):
+#                     time.sleep(2)
+#                     nltk.download('punkt')
+#                     rt = readtime.of_text(input_me)
+#                     tc = textstat.flesch_reading_ease(input_me)
+#                     tokenized_words = word_tokenize(input_me)
+#                     lr = len(set(tokenized_words)) / len(tokenized_words)
+#                     lr = round(lr,2)
+#                     n_s = textstat.sentence_count(input_me)
+#                     st.markdown('___')
+#                     st.text('Reading Time')
+#                     st.write(rt)
+#                     st.markdown('___')
+#                     st.text('Text Complexity: from 0 or negative (hard to read), to 100 or more (easy to read)')
+#                     st.write(tc)
+#                     st.markdown('___')
+#                     st.text('Lexical Richness (distinct words over total number of words)')
+#                     st.write(lr)
+#                     st.markdown('___')
+#                     st.text('Number of sentences')
+#                     st.write(n_s)
+#                     st.balloons()
+
+#     if source == 'I want to upload a file':
+#         file = st.file_uploader('Upload your file here',type=['txt'])
+#         if file is not None:
+#             with st.spinner('Processing...'):
+#                     time.sleep(2)
+#                     stringio = StringIO(file.getvalue().decode("utf-8"))
+#                     string_data = stringio.read()
+#                     if len(string_data) > 10000:
+#                         st.error('Please upload a file of maximum 10,000 characters')
+#                     else:
+#                         nltk.download('punkt')
+#                         rt = readtime.of_text(string_data)
+#                         tc = textstat.flesch_reading_ease(string_data)
+#                         tokenized_words = word_tokenize(string_data)
+#                         lr = len(set(tokenized_words)) / len(tokenized_words)
+#                         lr = round(lr,2)
+#                         n_s = textstat.sentence_count(string_data)
+#                         st.markdown('___')
+#                         st.text('Reading Time')
+#                         st.write(rt)
+#                         st.markdown('___')
+#                         st.text('Text Complexity: from 0 or negative (hard to read), to 100 or more (easy to read)')
+#                         st.write(tc)
+#                         st.markdown('___')
+#                         st.text('Lexical Richness (distinct words over total number of words)')
+#                         st.write(lr)
+#                         st.markdown('___')
+#                         st.text('Number of sentences')
+#                         st.write(n_s)
+#                         st.balloons()
 import streamlit as st
 import time
-from transformers import T5ForConditionalGeneration, T5Tokenizer
+from transformers import BartForConditionalGeneration, BartTokenizer
 import nltk
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
@@ -43,14 +189,14 @@ if nav == 'Summarize text':
             else:
                 with st.spinner('Processing...'):
                     time.sleep(2)
-                    model = T5ForConditionalGeneration.from_pretrained("t5-small")
-                    tokenizer = T5Tokenizer.from_pretrained("t5-small")
-                    inputs = tokenizer.encode("summarize: " + input_su, return_tensors="pt", max_length=512, truncation=True)
-                    summary_ids = model.generate(inputs, max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
-                    t5_summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+                    model = BartForConditionalGeneration.from_pretrained("facebook/bart-large-cnn")
+                    tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
+                    inputs = tokenizer(input_su, max_length=1024, return_tensors="pt", truncation=True)
+                    summary_ids = model.generate(inputs['input_ids'], num_beams=4, length_penalty=2.0, max_length=1500, min_length=100, early_stopping=True)
+                    bart_summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
                     st.markdown('___')
-                    st.write('T5 Model')
-                    st.success(t5_summary)
+                    st.write('BART Model')
+                    st.success(bart_summary)
                     st.balloons()
 
     if source == 'I want to upload a file':
@@ -63,14 +209,14 @@ if nav == 'Summarize text':
                     if len(string_data) < 1000 or len(string_data) > 10000:
                         st.error('Please upload a file between 1,000 and 10,000 characters')
                     else:
-                        model = T5ForConditionalGeneration.from_pretrained("t5-small")
-                        tokenizer = T5Tokenizer.from_pretrained("t5-small")
-                        inputs = tokenizer.encode("summarize: " + string_data, return_tensors="pt", max_length=512, truncation=True)
-                        summary_ids = model.generate(inputs, max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
-                        t5_summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+                        model = BartForConditionalGeneration.from_pretrained("facebook/bart-large-cnn")
+                        tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
+                        inputs = tokenizer(string_data, max_length=1024, return_tensors="pt", truncation=True)
+                        summary_ids = model.generate(inputs['input_ids'], num_beams=4, length_penalty=2.0, max_length=150, min_length=40, early_stopping=True)
+                        bart_summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
                         st.markdown('___')
-                        st.write('T5 Model')
-                        st.success(t5_summary)
+                        st.write('BART Model')
+                        st.success(bart_summary)
                         st.balloons()
 
 if nav == 'Analyze text':
@@ -89,7 +235,7 @@ if nav == 'Analyze text':
         input_me = st.text_area("Input your text in English (maximum of 10,000 characters)", value=a_example, height=330)
         if st.button('Analyze'):
             if len(input_me) > 10000:
-                st.error('Please enter a text in English of maximum 1,000 characters')
+                st.error('Please enter a text in English of maximum 10,000 characters')
             else:
                 with st.spinner('Processing...'):
                     time.sleep(2)
